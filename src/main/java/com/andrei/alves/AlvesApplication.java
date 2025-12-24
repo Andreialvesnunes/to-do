271085b9b595ebaba033d42988332b35aefea1f8
Andrei Alves <andreialvescvr2005@gmail.com>
2025-12-24 20:20:42 -0300

    Teste

---
+++ src/main/java/com/andrei/alves/AlvesApplication.java
@@ -0,0 +1,13 @@
+package com.andrei.alves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlvesApplication.class, args);
+	}
+
+}
